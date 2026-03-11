d7ac1a1692bd6b4e30cc76e97d7babd3cc54388e
katieypw <katieyipeiwu@gmail.com>
2026-03-11 18:00:35 -0400

    c5

--- w05/js/script.js
+++ w05/js/script.js
@@ -1,4 +1,4 @@
-$("#preamble-btn-1").click(function() {
+$("#preamble-btn-1").hover(function() {
     $("#preamble-btn-2").css({
         "background-color": "blue",
         "font-size": "1.5em",
@@ -9,19 +9,38 @@ $("#preamble-btn-1").click(function() {
 
 
 
-    $("#preamble-btn-2").click(function() {
+    $("#preamble-btn-2").hover(function() {
         $("#preamble-btn-1").css({
             "transform": "rotate(360deg)",
         
         });
     });
 
-$("#preamble-btn-2").click(function(){
+$("#preamble-btn-2").hover(function(){
 
     $("#preamble-btn-1").toggleClass("rotate")
 
 })
 
-$(".header-left").click(function(){
-    $("menu").toggleClass("active")
+$(".header-left").hover(function(){
+    $("#menu").toggleClass("active")
 })
+
+$(document).scroll(function(){
+    let distance=$(document).scrollTop()
+    let height = $(document).height()=($window).height();
+    let ratio = distance/height;
+
+   $(".header-right").html(ratio)
+    "transform"; "rotate"("360 + ratio")
+})
+
+
+$(".album-thumbs").flickity({
+    wrapAround: true,
+    pageDots: false
+
+})
+
+
+// fade and dots 
