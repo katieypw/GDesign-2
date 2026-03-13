3166dc9865dd81256831aa6e956ee315ad837b50
katieypw <katieyipeiwu@gmail.com>
2026-03-13 18:05:01 -0400

    cc

--- demos/02 - filtering/js/script.js
+++ demos/02 - filtering/js/script.js
@@ -1,4 +1,13 @@
-//leaflet
+//leaflet$
 //map libre
 //add custom marker
 //add cusgtom popup
+
+$(".filters button").click(function(){
+
+
+    f = $(this).data("year");
+    $(f).fadeToggle(500);
+
+    
+})
